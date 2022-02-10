01f3dfcb2ed06813755dfd2cc0ba2e4c7e0d4ccc
MarouaneZribi <MarouaneZribi@users.noreply.github.com>
2022-02-10 13:53:20 +0000

    ✅ Ready to clone and code.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import sys
 import pytest
 
-from project_name import create_app
-from project_name.ext.commands import populate_db
-from project_name.ext.database import db
+from testosint import create_app
+from testosint.ext.commands import populate_db
+from testosint.ext.database import db
 
 
 @pytest.fixture(scope="session")
